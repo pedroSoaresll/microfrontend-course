c7ed6aab03b720143b1906474a6f8bdf9415b0dc
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2023-01-29 12:26:26 -0300

    feat: prepare apps to production

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,27 +1,23 @@
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const devConfig = {
-  mode: 'development',
+  mode: "development",
   devServer: {
     port: 8080,
     historyApiFallback: {
-      index: 'index.html',
+      index: "index.html",
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: "container",
       remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 };
