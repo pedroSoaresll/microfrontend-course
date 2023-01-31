0a3eed43a664a3d107c117f5f1db158601a46af3
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2023-01-31 20:50:14 -0300

    fix: css class conflict

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
-import MarketingApp from './components/MarketingApp';
-import Header from './components/Header'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+})
 
 const router = createBrowserRouter([
   {
@@ -19,7 +23,7 @@ const router = createBrowserRouter([
 export default () => {
   return (
     <React.StrictMode>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <RouterProvider router={router} />
       </StylesProvider>
     </React.StrictMode>
